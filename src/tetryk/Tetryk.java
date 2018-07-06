05f63f0e4f3a308dad0b729762c56a2f6d60033e
mati <mati@mati-X542UQ>
2018-07-06 16:43:24 +0200

    Inicjalizacja repo

---
+++ src/tetryk/Tetryk.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetryk;
+
+/**
+ *
+ * @author mati
+ */
+public class Tetryk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
